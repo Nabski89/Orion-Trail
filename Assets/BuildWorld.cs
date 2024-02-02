31f9fb82775741abc97ac124899a51928a4f92e6
Nabski89 <Nabski@Hotmail.com>
2024-02-01 21:24:52 -0500

    More wiggle to the planet locations, additional beacon

--- Assets/BuildWorld.cs
+++ Assets/BuildWorld.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class BuildWorld : MonoBehaviour
 {
     public Transform LocationHolder;
+    public GameObject EventSetHolder;
+    public GameObject Beacon;
     public int EmptySpace = 5;
     public int HazardSpace = 5;
     public int MinorSpace = 5;
@@ -18,73 +20,114 @@ public class BuildWorld : MonoBehaviour
     int StoryCount = 0;
     // Start is called before the first frame update
     float SpawnX = 0f;
+    public float SpawnXOffsetPer = 20;
     float SpawnY = 0f;
     void Start()
     {
+        SpawnX = -SpawnXOffsetPer;
         while (StoryCount < StoryElement.Length)
         {
-            MoveLocationOver();
-            //the plus story count is so that we get more events later
-            int SpawnCount = Random.Range(1 + StoryCount / 3, 4 + StoryCount / 2);
-            int SpawnTotal = SpawnCount;
-            while (SpawnCount > 0)
-            {
-                SpawnY = SpawnTotal * 2 - SpawnCount * 3 + 1;
+            SpawnNonStory();
+            SpawnNonStory();
+            SpawnStory();
+        }
+        //activate all our children to enable the beacons
+        ActivateChildren(transform);
+    }
 
-                int EventType = Random.Range(0, EmptySpace + HazardSpace + MinorSpace + MajorSpace + LegendarySpace + 1);
-                switch (EventType)
-                {
-                    case int n when (n >= 0 && n < EmptySpace):
-                        Debug.Log("WorldBuilding: EmptySpace");
-                        break;
-
-                    case int n when (n >= EmptySpace && n < EmptySpace + HazardSpace):
-                        InstantiateFromArrays(Hazard);
-                        break;
-
-                    case int n when (n >= EmptySpace && n < EmptySpace + HazardSpace + MinorSpace):
-                        InstantiateFromArrays(Minor);
-                        break;
-
-                    case int n when (n >= EmptySpace && n < EmptySpace + HazardSpace + MinorSpace + MajorSpace):
-                        InstantiateFromArrays(Major);
-                        break;
-
-                    case int n when (n >= EmptySpace && n < EmptySpace + HazardSpace + MinorSpace + LegendarySpace):
-                        InstantiateFromArrays(StoryElement);
-                        break;
-                }
-                SpawnCount -= 1;
-            }
-
-
-            MoveLocationOver();
-            InstantiateFromArraysStory(StoryElement);
-            StoryCount += 1;
+    void ActivateChildren(Transform parent)
+    {
+        foreach (Transform child in parent)
+        {
+            child.gameObject.SetActive(true);
+            ActivateChildren(child);
         }
     }
 
+    private void SpawnNonStory()
+    {
+        MoveLocationOver();
+        //the plus story count is so that we get more events later
+        int SpawnCount = Random.Range(1 + StoryCount / 3, 4 + StoryCount / 2);
+        int SpawnTotal = SpawnCount;
+        GameObject CurrentEvent = Instantiate(EventSetHolder, LocationHolder);
+        while (SpawnCount > 0)
+        {
+            SpawnY = SpawnTotal * 8 - SpawnCount * 12;
 
-    private void InstantiateFromArrays(GameObject[] array)
+            int EventType = Random.Range(0, EmptySpace + HazardSpace + MinorSpace + MajorSpace + LegendarySpace + 1);
+            switch (EventType)
+            {
+                case int n when (n >= 0 && n < EmptySpace):
+                    Debug.Log("WorldBuilding: EmptySpace");
+                    break;
+
+                case int n when (n >= EmptySpace && n < EmptySpace + HazardSpace):
+                    InstantiateFromArrays(Hazard, CurrentEvent);
+                    break;
+
+                case int n when (n >= EmptySpace && n < EmptySpace + HazardSpace + MinorSpace):
+                    InstantiateFromArrays(Minor, CurrentEvent);
+                    break;
+
+                case int n when (n >= EmptySpace && n < EmptySpace + HazardSpace + MinorSpace + MajorSpace):
+                    InstantiateFromArrays(Major, CurrentEvent);
+                    break;
+
+                case int n when (n >= EmptySpace && n < EmptySpace + HazardSpace + MinorSpace + LegendarySpace):
+                    InstantiateFromArrays(StoryElement, CurrentEvent);
+                    break;
+            }
+            SpawnCount -= 1;
+        }
+    }
+    private void SpawnStory()
+    {
+        GameObject CurrentEvent = Instantiate(EventSetHolder, LocationHolder);
+        MoveLocationOver();
+        InstantiateFromArraysStory(StoryElement, CurrentEvent);
+        StoryCount += 1;
+    }
+    private void InstantiateFromArrays(GameObject[] array, GameObject SpawnLocation)
     {
         // Randomly choose an object from the array
         GameObject chosenObject = array[Random.Range(0, array.Length)];
         Debug.Log("WorldBuilding: " + chosenObject);
         // Instantiate the chosen object
-        Instantiate(chosenObject, new Vector3(SpawnX + Random.Range(-0.15f, 0.15f), SpawnY + Random.Range(-0.15f, 0.15f), -0.5f), Quaternion.identity, LocationHolder);
+        GameObject EventSpawned = Instantiate(chosenObject, new Vector3(SpawnX + Random.Range(-SpawnXOffsetPer/4, SpawnXOffsetPer/4), SpawnY + Random.Range(-2f, 2f), -0.5f), Quaternion.identity, SpawnLocation.transform);
+        //give it at least one beacon, maybe more
+        Instantiate(Beacon, EventSpawned.transform.position, Quaternion.identity, EventSpawned.transform);
+
+        if (Random.Range(0, 2) > 0)
+        {
+            Instantiate(Beacon, EventSpawned.transform.position, Quaternion.identity, EventSpawned.transform);
+            if (Random.Range(0, 2) > 0)
+                Instantiate(Beacon, EventSpawned.transform.position, Quaternion.identity, EventSpawned.transform);
+        }
+
+
     }
-    private void InstantiateFromArraysStory(GameObject[] array)
+    private void InstantiateFromArraysStory(GameObject[] array, GameObject SpawnLocation)
     {
         // Randomly choose an object from the array
         GameObject chosenObject = array[StoryCount];
 
         // Instantiate the chosen object
-        Instantiate(chosenObject, new Vector3(SpawnX, SpawnY + Random.Range(-2.15f, 2.15f), -0.5f), Quaternion.identity, LocationHolder);
+        GameObject EventSpawned = Instantiate(chosenObject, new Vector3(SpawnX + Random.Range(-SpawnXOffsetPer/3, SpawnXOffsetPer/3), SpawnY + Random.Range(-10f, 10f), -0.5f), Quaternion.identity, SpawnLocation.transform);
+        //give it at least one beacon, maybe more
+        Instantiate(Beacon, EventSpawned.transform.position, Quaternion.identity, EventSpawned.transform);
+
+        if (Random.Range(0, 2) > 0)
+        {
+            Instantiate(Beacon, EventSpawned.transform.position, Quaternion.identity, EventSpawned.transform);
+            if (Random.Range(0, 2) > 0)
+                Instantiate(Beacon, EventSpawned.transform.position, Quaternion.identity, EventSpawned.transform);
+        }
     }
 
     private void MoveLocationOver()
     {
-        SpawnX += 6;
+        SpawnX += SpawnXOffsetPer;
         SpawnY = 0;
     }
 }
--- Assets/CreateBeaconPath.cs
+++ Assets/CreateBeaconPath.cs
@@ -8,10 +8,15 @@ public class CreateBeaconPath : MonoBehaviour
     public Vector2 PointA;
     public Vector2 PointB;
     public float distanceBetweenBeacons = 2f;
+    public Color[] BeaconColorOptions;
     public Color BeaconColor = Color.white;
 
     void Start()
     {
+        BeaconColor = BeaconColorOptions[Random.Range(0, BeaconColorOptions.Length)];
+        PointA = transform.parent.transform.position;
+        GPTChildFinding();
+        PointB = nextSibling.GetChild(Random.Range(0, nextSibling.transform.childCount)).transform.position;
         InstantiateBeaconsAlongLine(PointA, PointB, distanceBetweenBeacons);
     }
 
@@ -26,8 +31,42 @@ public class CreateBeaconPath : MonoBehaviour
             Vector2 BeaconSpawn = Vector2.Lerp(start, end, t);
             BeaconSpawn.x += Random.Range(-0.5f, 0.5f);
             BeaconSpawn.y += Random.Range(-0.5f, 0.5f);
-            Instantiate(BeaconPrefab, new Vector3(BeaconSpawn.x, BeaconSpawn.y, 0f), Quaternion.identity, transform);
+            //the .75 is to get it above some other stuff
+            Instantiate(BeaconPrefab, new Vector3(BeaconSpawn.x, BeaconSpawn.y, -0.75f), Quaternion.identity, transform);
             start = BeaconSpawn;
+        }
+    }
+
+
+    private int childIndex;
+    private int siblingCount;
+    private Transform nextSibling;
+    void GPTChildFinding()
+    {
+        // Get the parent of this object
+        Transform parent = transform.parent.parent.parent;
+
+        // Get the total number of siblings
+        siblingCount = parent.childCount;
+
+        // Loop through the siblings to find the index of this object
+        for (int i = 0; i < siblingCount; i++)
+        {
+            if (parent.GetChild(i) == transform.parent.parent)
+            {
+                childIndex = i;
+                break;
+            }
+        }
+        // Set the next sibling
+        if (childIndex < siblingCount - 1)
+        {
+            nextSibling = parent.GetChild(childIndex + 1);
+        }
+        else
+        {
+            // If this is the last child, set nextSibling to null
+            nextSibling = null;
         }
     }
 }
