3c7d96a159d07c09871f5feb6d95695b82db3e50
Nabski89 <Nabski@Hotmail.com>
2024-09-14 09:50:04 -0400

    Slot-o-rama

--- Assets/SlotMachineManager.cs
+++ Assets/SlotMachineManager.cs
@@ -1,17 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SlotMachineManager : MonoBehaviour
 {
     public SlotMachineRoll[] slotMachineRolls;
-
+    public GenericManager genericManager;
     void Start()
     {
+        genericManager = GetComponentInParent<GenericManager>();
         // Get all SlotMachineRoll components in the children
+
         slotMachineRolls = GetComponentsInChildren<SlotMachineRoll>();
     }
-
+    public CharacterBulkManager CrewShipReference;
+    void ActivateSlots()
+    {
+        CharacterManager[] characterManagers = CrewShipReference.GetComponentsInChildren<CharacterManager>();
+        // Perform an action for each element in the array
+        for (int i = 0; i < slotMachineRolls.Length; i++)
+        {
+            slotMachineRolls[i].ColorImage1.color = characterManagers[i].CharacterColor[0];
+            slotMachineRolls[i].ColorImage2.color = characterManagers[i].CharacterColor[1];
+        }
+    }
     // Update is called once per frame
     void Update()
     {
@@ -19,6 +32,11 @@ public class SlotMachineManager : MonoBehaviour
     }
     public void StartRoll()
     {
+        ActivateSlots();
+        Selectable = 1;
+        Atk = 0;
+        Block = 0;
+        Special = 0;
         foreach (SlotMachineRoll roll in slotMachineRolls)
             roll.StartRouletteButton();
     }
@@ -34,13 +52,21 @@ public class SlotMachineManager : MonoBehaviour
     public int Atk;
     public int Block;
     public int Special;
-    public int Selectable = 3;
+    public int Selectable = 1;
     public void Select(int Selected)
     {
-        Selectable -= 1;
-        SlotValue Read = slotMachineRolls[Selected].transform.GetChild(1).GetComponent<SlotValue>();
-        Atk += Read.Attack;
-        Block += Read.Block;
-        Special += Read.Buff;
+        if (Selectable > 0)
+        {
+            Selectable -= 1;
+            foreach (SlotMachineRoll SlotCharacter in slotMachineRolls)
+            {
+                SlotValue Read = SlotCharacter.transform.GetChild(Selected).GetComponent<SlotValue>();
+                Atk += Read.Attack;
+                Block += Read.Block;
+                Special += Read.Buff;
+
+            }
+            genericManager.MainTextReference.TEXTBOX += "<br>Attack for " + Atk + ". Block for " + Block + ". Special count of " + Special;
+        }
     }
 }
