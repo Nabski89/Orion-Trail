cfe267fbafce19af5376628dc78c6ad20b0e76af
Nabski89 <Nabski@Hotmail.com>
2024-10-05 11:15:59 -0400

    More Skills

---
+++ Assets/TraitSpecies.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TraitSpecies : MonoBehaviour, ITrait
+{
+    [SerializeField]
+    private int traitNumber;
+
+    public int TraitNumber
+    {
+        get { return traitNumber; }
+    }
+    [SerializeField]
+    private Sprite traitIcon;
+
+    public Sprite TraitIcon
+    {
+        get { return traitIcon; }
+    }
+
+    [SerializeField]
+    private string traitName;
+    public string TraitName
+    {
+        get { return traitName; }
+    }
+
+    [SerializeField]
+    private string traitDescription;
+    public string TraitDescription
+    {
+        get { return traitDescription; }
+    }
+    // Start is called before the first frame update
+    public void CombatBonus()
+    {
+
+    }
+
+    // Update is called once per frame
+    public void SkillBonus()
+    {
+
+    }
+}
